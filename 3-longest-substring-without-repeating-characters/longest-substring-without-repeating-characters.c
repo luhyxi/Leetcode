caa5d051348b15fd62327a62e57838b54ffe4d3b
LUANA HYPARXIS <125469882+luhyxi@users.noreply.github.com>
2023-07-29 12:36:11 -0300

    Time: 3 ms (88.57%) | Memory: 5.9 MB (29.16%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.c
@@ -0,0 +1,39 @@
+/* PUTA QUE PARIU QUE PROGRAMA FEIO!!!!!!!!!
+    PROGRAMA MAIS FEIO QUE EU JÁ ESCREVI!! */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXLEN 50000
+
+int lengthOfLongestSubstring(char * s){
+    if (*s == '\0') return 0; // test case de merda
+
+    int s_Len = strlen(s); int i;
+    int size_Sub = 0; int Array_s[MAXLEN];
+    int char_set[128];
+
+    
+    // Alocar as substrings (como int) dentro de um array
+    int n = 0;
+    memset(char_set, 0, sizeof(char_set));
+    char *start = s, *end = s;
+    for (i = 0; i < s_Len; i++){
+        if (char_set[*end] == 0) {
+            char_set[*end] = 1;
+            size_Sub = end - start + 1;} else {
+                while(*start != *end) {
+                    char_set[*start] = 0; start++;}
+                start ++;}
+        Array_s[n] = size_Sub;
+        n++; end++;}
+
+    // Encontra a maior Substring
+    int max = Array_s[0];
+    for (i = 1; i < n; i++){
+        if (Array_s[i] > max){
+            max = Array_s[i];}
+        } return max;
+}
