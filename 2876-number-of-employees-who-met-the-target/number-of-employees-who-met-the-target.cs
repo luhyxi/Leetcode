b410df63803249db6678cb0a2a5a4f3d73677289
LUANA HYPARXIS <125469882+luhyxi@users.noreply.github.com>
2023-08-03 21:19:59 -0300

    Time: 78 ms (67.40%) | Memory: 40.8 MB (10.57%) - LeetSync

---
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.cs
@@ -0,0 +1,8 @@
+public class Solution
+{
+    public int NumberOfEmployeesWhoMetTarget(int[] hours, int target)
+    {
+        var result = Array.FindAll(hours, element => element >= target);
+        return result.Length;
+    } 
+}
