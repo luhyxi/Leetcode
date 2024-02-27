76741800579ee4b5fe61c433d52dc2473ca684c1
Luana Ritter <125469882+luhyxi@users.noreply.github.com>
2024-02-27 14:13:57 -0300

    Time: 249 ms (61.39%), Space: 61.7 MB (79.06%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int MaxArea(int[] height)
+        {
+            int maxArea = 0;
+            int left = 0;
+            int right = height.Length - 1;
+
+            while (left < right)
+            {
+                int currentArea = Math.Min(height[left], height[right]) * (right - left);
+                maxArea = Math.Max(maxArea, currentArea);
+
+                if (height[left] < height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return maxArea;
+        }
+    }
